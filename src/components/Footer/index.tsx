e9e775ba21ab58cfa0617f24e366fcd714fa5c36
Mu-jun <suo24691@gmail.com>
2025-12-03 21:47:17 +0900

    이메일 제거

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,7 @@ function Footer() {
             더청년컨설팅연대 Seocho-gu, Seoul, 17 Block 77 Building,
             Seocho-daero 77-gil
           </p>
-          <p>E-mail: 010-4668-0739</p>
+          {/* <p>E-mail: 010-4668-0739</p> */}
           <p>TEL: 010-4668-0739</p>
         </div>
         <p>Copyright © 더청년컨설팅연대. All rights reserved.</p>
